5c6ed822f908930cbd5461308d8eeb1b0e981801
swati2904 <swati.lpu17@gmail.com>
2021-05-24 21:24:01 +0530

    event handling, changing state & passing state to child component

--- Part-1/Practice Folder/part-c/src/App.js
+++ Part-1/Practice Folder/part-c/src/App.js
@@ -1,20 +1,144 @@
-//props directly passed to the component are 
-//now directly destructured into the variable name and age 
-// ***********************************************************
-const Hello = ({name, age}) => {
-  const bornYear = () => new Date().getFullYear() - age
+import React, { useState } from 'react'
+
+// Refactoring the components
+// *****************************************************
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+//Passing state to child components
+// *****************************************************
+
+// const Display = (props) => {
+//   return (
+//     <div>{props.counter}</div>
+//   )
+// }
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
-    <div> 
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>
-        So you were probably born in <b> {bornYear()} </b>
-      </p>
+    <div>
+      <Display counter={counter} />
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />
     </div>
   )
 }
+// const Button = (props) => {
+//   return (
+//     <button onClick={props.handleClick}>
+//       {props.text}
+//     </button>
+//   )
+// }
+
+//separate the event handlers into separate functions
+// *****************************************************
+
+// const App = () => {
+//   const [ counter, setCounter ] = useState(0)
+
+//   const increaseByOne = () => setCounter(counter + 1)
+
+//   const setToZero = () => setCounter(0)
+
+//   return (
+//     <div>
+//       <div>{counter}</div>
+//       <button onClick={increaseByOne}>
+//         set
+//       </button>
+//       <button onClick={setToZero}>
+//         reset
+//       </button>
+//     </div>
+//   )
+// }
+
+
+//Event handling
+// *****************************************************
+
+// const App = () => {
+//   const [counter, setCounter] = useState(0)
+
+//   const handleClick = () => {
+//     console.log('clicked')
+//   }
+
+//   return (
+//     <div> 
+//       <div> {counter}</div>
+//       <button onClick={() => setCounter(counter+1)}>
+//         Plus
+//       </button>
+//       <button onClick={() => setCounter(0)}>
+//         reset
+//       </button>
+//     </div>
+//   )
+// }
+
+
+
+
+// Stateful component
+// *****************************************************
+
+// const App = () => {
+//   const [counter, setCounter] = useState(0)
+
+//   setTimeout(
+//     ()=>{
+//       return setCounter (counter + 1),
+//       1000
+//     }
+//   )
+//   return (
+//     <div> {counter}</div>
+//   )
+// }
+
+
+
+
+//props directly passed to the component are 
+//now directly destructured into the variable name and age 
+// ***********************************************************
+
+// const Hello = ({name, age}) => {
+//   const bornYear = () => new Date().getFullYear() - age
+
+//   return (
+//     <div> 
+//       <p>
+//         Hello {name}, you are {age} years old
+//       </p>
+//       <p>
+//         So you were probably born in <b> {bornYear()} </b>
+//       </p>
+//     </div>
+//   )
+// }
 
 // destructuring 
 // ***********************************************************
@@ -54,17 +178,18 @@ const Hello = ({name, age}) => {
 //   )
 // }
 
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Swati" age={26 - 4} />
-      <Hello name={name} age={age} />
-    </div>
-  )
-}
+// const App = (props) => {
+//   const name = 'Peter'
+//   const age = 10
+//   const {counter} = props
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+//       <Hello name="Swati" age={26 - 4} />
+//       <Hello name={name} age={age} />
+//       <div>{counter}</div>
+//     </div>
+//   )
+// }
 
 export default App;
--- Part-1/Practice Folder/part-c/src/index.js
+++ Part-1/Practice Folder/part-c/src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
 
+
+// let counter = 1
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App/>,
+document.getElementById('root')
 );
 
+// after first time page rendered and counter value goes to the one 
+// it will not updated for the second time (hen re-rendered the page).
+// counter +=1
 
+// const refresh = () => {
+// ReactDOM.render(
+//     <App counter={counter}/>,
+//   document.getElementById('root')
+// );
+// }
+// refresh()
+// counter += 1
+// refresh()
+// counter += 1
+// refresh()
