53f30a762b746f2247a476fb86ff4ccae5f7b1fa
swati2904 <swati.lpu17@gmail.com>
2021-05-28 20:43:19 +0530

    form example

--- Part-2/Practice Folder/part-a/src/App.js
+++ Part-2/Practice Folder/part-a/src/App.js
@@ -1,5 +1,55 @@
-import React from 'react'
-import Note from './components/Note'
+import React, { useState } from "react";
+import Note from "./components/Note";
+
+//Forms
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+        <form onSubmit={addNote}>
+          <input value={newNote} onChange={handleNoteChange} />
+          <button type="submit">Save</button>
+        </form>
+      </ul>
+    </div>
+  );
+};
 
 //destructring
 // *******************
@@ -10,18 +60,18 @@ import Note from './components/Note'
 //   )
 // }
 
-const App = ({notes}) => {
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map((note) => 
-        <Note key={note.id} note={note} />
-        )} 
-      </ul>
-    </div>
-  )
-}
+// const App = ({notes}) => {
+//   return (
+//     <div>
+//       <h1>Notes</h1>
+//       <ul>
+//         {notes.map((note) =>
+//         <Note key={note.id} note={note} />
+//         )}
+//       </ul>
+//     </div>
+//   )
+// }
 
 // const App = (props) => {
 //   const { notes } = props
@@ -40,4 +90,4 @@ const App = ({notes}) => {
 //   )
 // }
 
-export default App
+export default App;
