0002aa234cb8f0d95504a287a4a1439a49f98208
swati2904 <swati.lpu17@gmail.com>
2021-05-30 23:45:48 +0530

    sending data to the server

--- Part-2/Practice Folder/part-c/src/App.js
+++ Part-2/Practice Folder/part-c/src/App.js
@@ -1,59 +1,40 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import Note from './components/Note'
-
+import React, { useState, useEffect } from "react";
+import Note from "./components/Note";
+import axios from "axios";
 const App = () => {
-  const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(true)
-
-  const hook = () => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-  }
-  
-  useEffect(hook, [])
-
-  // we can also write this............
-//   useEffect(() => {
-//   console.log('effect')
-//   axios
-//     .get('http://localhost:3001/notes')
-//     .then(response => {
-//       console.log('promise fulfilled')
-//       setNotes(response.data)
-//     })
-// }, [])
-
-  console.log('render', notes.length, 'notes')
-
-  const notesToShow = showAll
-    ? notes
-    : notes.filter((note) => note.important === true);
-
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(false);
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
+    });
+  }, []);
+  console.log("render", notes.length, "notes");
   const addNote = (event) => {
     event.preventDefault();
     const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
-      important: Math.random() < 0.5,
+      important: Math.random() > 0.5,
       id: notes.length + 1,
     };
 
     setNotes(notes.concat(noteObject));
     setNewNote("");
+    axios.post("http://localhost:3001/notes", noteObject).then((response) => {
+      setNotes(notes.concat(response.data));
+      setNewNote("");
+    });
   };
 
   const handleNoteChange = (event) => {
     console.log(event.target.value);
     setNewNote(event.target.value);
   };
-
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
   return (
     <div>
       <h1>Notes</h1>
@@ -66,13 +47,12 @@ const App = () => {
         {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
-        <form onSubmit={addNote}>
-          <input value={newNote} onChange={handleNoteChange} />
-          <button type="submit">Save</button>
-        </form>
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
-}
-
+};
 export default App;
