0dfba008244022f817a7ff36594b532bf24ae187
swati2904 <swati.lpu17@gmail.com>
2021-06-02 18:48:19 +0530

    3.5: Phonebook backend step5

--- Part-3/index.js
+++ Part-3/index.js
@@ -53,6 +53,29 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
+const generateId = () => {
+  return Math.floor(Math.random() * 100);
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: String(body.number),
+  };
+
+  persons = persons.concat(person);
+  response.json(person);
+});
+
 app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   persons = persons.filter((person) => person.id !== id);
